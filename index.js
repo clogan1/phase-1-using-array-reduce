1d7d3530bc379ce06b2b2ce9def76797815ec900
Claire <clairelogan16@gmail.com>
2021-07-05 16:24:47 -0700

    commit tree 4ba04300a92425df8f8cb84388b731264aea7835

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+//const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+
+// const totalBatteries = batteryBatches.reduce(function(accumulator, element) { return element + accumulator},0);
+
+
+
+
+
+
+
+
+//const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+
+// const totalBatteries = batteryBatches.reduce((accum, batch) => {
+//     return accum + batch}, 0);
+
+
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-
-const totalBatteries = batteryBatches.reduce(function(accumulator, element) { return element + accumulator},0);
+const totalBatteries = batteryBatches.reduce((bat,total) => bat + total ,0);
